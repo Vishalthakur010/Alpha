4feb5330d514915a03c8707a0a505345dd226736
vishal thakur <vishalthakur970820@gmail.com>
2024-06-04 20:03:18 +0530

    array

---
+++ Recursion/sort_array.java
@@ -0,0 +1,16 @@
+public class sort_array {
+    public static boolean sorted(int arr[], int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+        return sorted(arr, i+1);
+    }
+    public static void main(String[] args){
+        int arr[]= {1,2,3,4,5};
+        int i=arr[0];
+        System.out.println(sorted(arr, i));
+    }
+}
